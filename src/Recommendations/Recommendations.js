3d45231266bab162e650281cf85b6ae6eaa464c9
amysimmons <amy__simmons@hotmail.com>
2015-11-16 21:58:12 +1100

    findings highlights added

--- src/Recommendations/Recommendations.js
+++ src/Recommendations/Recommendations.js
@@ -14,15 +14,41 @@ var Recommendation = React.createClass({
 });
 
 var Recommendations = React.createClass({
+  recommendationOneDescription(){
+    return (
+      <div>
+        <p>Weekly one on one <span className="recommendation-highlight">sessions with a senior</span> mentor or buddy.</p>
+      </div>
+    )
+  },
+  recommendationTwoDescription(){
+    return (
+      <div>
+        <p>Weekly <span className="recommendation-highlight">pair programming</span> and <span className="recommendation-highlight">code reviews</span>.</p>
+      </div>
+    )
+  },
+  recommendationThreeDescription(){
+    return (
+      <div>
+        <p>Allocated work <span className="recommendation-highlight">time to learn</span> all the things.</p>
+      </div>
+    )
+  },
+
   render(){
+    var recommendationOneDescription = this.recommendationOneDescription();
+    var recommendationTwoDescription = this.recommendationTwoDescription();
+    var recommendationThreeDescription = this.recommendationThreeDescription();
+
     return (
       <div className="recommendations-container">
         <div className="recommendations-heading">
           <h2>Recommendations</h2>
         </div>
-        <Recommendation description={"One on ones with a mentor/buddy"}/>
-        <Recommendation description={"Pair programming & code reviews"}/>
-        <Recommendation description={"Time to learn all the things"}/>
+        <Recommendation description={recommendationOneDescription}/>
+        <Recommendation description={recommendationTwoDescription}/>
+        <Recommendation description={recommendationThreeDescription}/>
       </div>
     )
   }
