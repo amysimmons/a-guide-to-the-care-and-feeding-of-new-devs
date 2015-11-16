92f63fe1cc16940730756d3965d17a553360477d
amysimmons <amy__simmons@hotmail.com>
2015-11-16 19:34:44 +1100

    responsive styling started

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -10,7 +10,8 @@ var Footer = React.createClass({
 	  		<div className="footer-about-heading">
 	  			<h2>About the data</h2>
 	  			<p>This data was collected via an <a href="https://docs.google.com/forms/d/1Dit-eI7Apd9_PQAK0yIumJVyeNQyOR_zWDH9UxN0fgg/viewform?c=0&w=1">online survey</a> over a two-week period in November 2015.
-	  			The stories are unedited. Overseas responses were not included in the findings, to ensure an accurate portrayal of the Australian tech industry.</p>
+	  			The stories are unedited. Overseas responses were not included in the findings, to ensure an accurate portrayal of the Australian tech industry.
+	  			The survey was conducted as research for a talk that I was asked to give at <a href="http://vi.campjs.com/sessions.html#sdwmyf">CampJS</a> on November 21, 2015.</p>
 	  	
 	  		</div>
 	  	</div>
